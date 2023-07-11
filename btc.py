7dcda78bd65d41378850a78e9421834e02e428a4
MaxLsiboa <131786808+MaxLsiboa@users.noreply.github.com>
2023-07-11 00:52:35 -0300

    Update btc.py

--- btc.py
+++ btc.py
@@ -1,7 +1,6 @@
 from telegram import Bot, Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from binance.client import Client as BinanceClient
-import utf8_locale
 
 # Configuração do Telegram
 TELEGRAM_TOKEN = '6002886491:AAHWbdB2DyExhvnIWJOQxkd26eP7FDCP75Q'
@@ -14,38 +13,36 @@ client = BinanceClient(BINANCE_API_KEY, BINANCE_API_SECRET)
 # Armazena o último preço verificado do Bitcoin
 last_price = None
 
-def format_brl_value(value):
-    value_float = float(value)
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-    formatted_value = locale.currency(value_float, grouping=True, symbol='R$')
-    return f" {formatted_value} "
+def format_usd_value(value):
+    return "{:,.2f} USD".format(value)
 
 def format_btc_value(value):
-    return f"{value:.8f}"
+    return "{:.8f} BTC".format(value)
+
+def format_brl_value(value):
+    return "R$ {:.2f}".format(value)
 
 def check_price(context):
     global last_price
     job = context.job
-    btc_price = float(client.get_avg_price(symbol="BTCBRL")['price'])
+    btc_price = float(client.get_avg_price(symbol="BTCUSDT")['price'])
     if last_price is not None:
         if btc_price > last_price:
-            message = f"O preço do Bitcoin subiu para {format_brl_value(btc_price)}. Vamos vender agora?"
+            message = f"O preço do Bitcoin subiu para {format_usd_value(btc_price)}. Devemos vender agora?"
         elif btc_price < last_price:
-            message = f"O preço do Bitcoin baixou para {format_brl_value(btc_price)}. Vamos comprar agora?"
+            message = f"O preço do Bitcoin caiu para {format_usd_value(btc_price)}. Devemos comprar agora?"
         else:
-            message = f"O preço do Bitcoin se manteve em {format_brl_value(btc_price)}."
+            message = f"O preço do Bitcoin se manteve em {format_usd_value(btc_price)}."
         context.bot.send_message(chat_id=job.context, text=message)
     last_price = btc_price
 
 def start(update: Update, context):
-    global chat_id
-    chat_id = update.effective_chat.id
     chat_id = update.effective_chat.id
     bot = context.bot
-    btc_price = client.get_avg_price(symbol="BTCBRL")['price']
-    usdt_price = client.get_avg_price(symbol="USDTBRL")['price']
-    message = f"Olá! Sou um robô estou aqui para te ajudar com o valor do BTC.\n"
-    message += f"O preço atual do Bitcoin é {format_brl_value(btc_price)} BRL e o preço do Dólar é {format_brl_value(usdt_price)} BRL.\n"
+    btc_price = float(client.get_avg_price(symbol="BTCUSDT")['price'])
+    usdt_price = float(client.get_avg_price(symbol="USDTBRL")['price'])
+    message = f"Olá! Sou um robô aqui para ajudar com o valor do BTC.\n"
+    message += f"O preço atual do Bitcoin é {format_usd_value(btc_price)} e o preço do USDT é {format_brl_value(usdt_price)}.\n"
     message += "Você gostaria de investir hoje?"
     context.bot.send_message(chat_id=chat_id, text=message)
 
@@ -53,8 +50,8 @@ def invest(update: Update, context):
     chat_id = update.effective_chat.id
     message_text = update.message.text.lower()
 
-    if message_text == 'não' or message_text == 'agradecido':
-        message = "Tudo bem, por nada! Estou aqui para ajudar. Se tiver alguma outra pergunta, é só me perguntar!"
+    if message_text == 'não' or message_text == 'obrigado':
+        message = "Tudo bem, de nada! Estou aqui para ajudar. Se tiver mais alguma pergunta, é só me perguntar!"
         context.bot.send_message(chat_id=chat_id, text=message)
     else:
         try:
@@ -63,42 +60,26 @@ def invest(update: Update, context):
             usdt_price = float(client.get_avg_price(symbol="USDTBRL")['price'])
             btc_quantity = investment_value / btc_price
             usdt_quantity = investment_value / usdt_price
+
+            if btc_quantity is not None:
+                btc_formatted = format_btc_value(btc_quantity)
+            else:
+                btc_formatted = "Valor inválido"
+
             message = f"Com o valor de {format_brl_value(investment_value)}, você adquirirá:\n"
-            message += f"{format_btc_value(btc_quantity)} BTC\n"
+            message += f"{btc_formatted}\n"
             message += f"{format_brl_value(usdt_quantity)} USDT\n"
-            message += "Compre Bitcoin e Dólar sem taxa de câmbio aqui no Binance.com."
+            message += "Compre Bitcoin e USDT sem taxa de câmbio aqui na Binance.com."
             context.bot.send_message(chat_id=chat_id, text=message)
 
-            keyboard = [[InlineKeyboardButton("Binance.com", url="https://www.binance.com/pt-BR/activity/referral-entry/CPA?ref=CPA_00YRJDOZZV")]]
+            keyboard = [[InlineKeyboardButton("Binance.com", url="https://www.binance.com/")]]
             reply_markup = InlineKeyboardMarkup(keyboard)
 
-            message = "Clique no botão abaixo para acessar o Binance.com:"
+            message = "Clique no botão abaixo para acessar a Binance.com:"
             context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
         except ValueError:
-            message = "Por favor, digite um valor válido para investimento."
+            message = "Por favor, digite um valor válido para o investimento."
             context.bot.send_message(chat_id=chat_id, text=message)
-
-def notify():
-    chat_id = globals().get('chat_id')
-
-    btc_price = float(client.get_avg_price(symbol="BTCBRL")['price'])
-    if btc_price > last_price:
-        message = f"O preço do Bitcoin subiu para {format_brl_value(btc_price)} BRL. Vamos vender agora?"
-    elif btc_price < last_price:
-        message = f"O preço do Bitcoin baixou para {format_brl_value(btc_price)} BRL. Vamos comprar agora?"
-    else:
-        return  # Não enviar notificação se o preço não mudou
-
-    context.bot.send_message(chat_id=chat_id, text=message)
-    last_price = btc_price
-
-    # Agendar a função notify() para ser executada a cada hora
-    schedule.every(1).hours.do(notify)
-
-    # Executar o agendador em segundo plano
-    while True:
-             schedule.run_pending()
-             time.sleep(1)   
 
 def main():
     bot = Bot(token=TELEGRAM_TOKEN)
@@ -110,7 +91,7 @@ def main():
     updater.dispatcher.add_handler(start_handler)
     updater.dispatcher.add_handler(invest_handler)
 
-    # Agenda a verificação do preço a cada hora
+    # Agendar a verificação do preço a cada hora
     job_queue = updater.job_queue
     job_queue.run_repeating(check_price, interval=3600, first=0, context=updater.bot.get_me().id)
 
@@ -118,5 +99,4 @@ def main():
     updater.idle()
 
 if __name__ == '__main__':
-
     main()
