ce85c87429628f37052bc0e9c8ebbcff02019ba7
Maxlsiboa <elligiomax@gmail.com>
2023-07-10 22:47:54 -0300

    Adicionar pacote binance ao requirements.txt

--- btc.py
+++ btc.py
@@ -1,17 +1,7 @@
-from telegram import Bot
-from telegram import Update
-from queue import Queue
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
-from binance import Client as BinanceClient
-import requests
-import httpx
-import decimal
-
-def get_bot_info(bot_token):
-    url = f"https://api.telegram.org/bot{bot_token}/getMe"
-    response = requests.get(url)
-    data = response.json()
-    return data
+from telegram import Bot, Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from binance.client import Client as BinanceClient
+import locale
 
 # Configuração do Telegram
 TELEGRAM_TOKEN = '6002886491:AAHWbdB2DyExhvnIWJOQxkd26eP7FDCP75Q'
@@ -21,121 +11,112 @@ BINANCE_API_KEY = "8jhntBVKAH5rbUmOcm7veI2O4ZND3sLyyA3ZYcwtNPcgJik1jWN28gNh8m8cK
 BINANCE_API_SECRET = "BZRgwAmarrXCFmiB1Yup7SHXPQOOuETTBiYwzw1QZVx4OdpdOvXUIEruLaB6DoFM"
 client = BinanceClient(BINANCE_API_KEY, BINANCE_API_SECRET)
 
-# Números de telefone cadastrados
-numeros_cadastrados = {}
+# Armazena o último preço verificado do Bitcoin
+last_price = None
 
-def cadastrar_numero(numero):
-    if numero in numeros_cadastrados:
-        return False
-    numeros_cadastrados[numero] = {
-        "state": "inicio",
-        "confirmado": False
-    }
-    return True
-
-def confirmar_acesso(numero):
-    if numero not in numeros_cadastrados:
-        return False
-    numeros_cadastrados[numero]["confirmado"] = True
-    return True
-
-def get_binance_price(symbol):
-    try:
-        price_ticker = client.get_symbol_ticker(symbol=symbol)
-        return price_ticker['price']
-    except:
-        return None
+def format_brl_value(value):
+    value_float = float(value)
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    formatted_value = locale.currency(value_float, grouping=True, symbol='R$')
+    return f" {formatted_value} "
 
 def format_btc_value(value):
     return f"{value:.8f}"
 
-def format_brl_value(value):
-    return f"R${value:,.2f}"
-
-def format_usd_value(value):
-    return f"${value:,.2f}"
-
-def calculate_investment(investment_value, price):
-    invested = decimal.Decimal(investment_value) / decimal.Decimal(price)
-    return invested.quantize(decimal.Decimal('0.00000000'))
+def check_price(context):
+    global last_price
+    job = context.job
+    btc_price = float(client.get_avg_price(symbol="BTCBRL")['price'])
+    if last_price is not None:
+        if btc_price > last_price:
+            message = f"O preço do Bitcoin subiu para {format_brl_value(btc_price)}. Vamos vender agora?"
+        elif btc_price < last_price:
+            message = f"O preço do Bitcoin baixou para {format_brl_value(btc_price)}. Vamos comprar agora?"
+        else:
+            message = f"O preço do Bitcoin se manteve em {format_brl_value(btc_price)}."
+        context.bot.send_message(chat_id=job.context, text=message)
+    last_price = btc_price
 
 def start(update: Update, context):
+    global chat_id
     chat_id = update.effective_chat.id
-    message = "Olá! Sou um bot do Telegram."
+    chat_id = update.effective_chat.id
+    bot = context.bot
+    btc_price = client.get_avg_price(symbol="BTCBRL")['price']
+    usdt_price = client.get_avg_price(symbol="USDTBRL")['price']
+    message = f"Olá! Sou um robô estou aqui para te ajudar com o valor do BTC.\n"
+    message += f"O preço atual do Bitcoin é {format_brl_value(btc_price)} BRL e o preço do Dólar é {format_brl_value(usdt_price)} BRL.\n"
+    message += "Você gostaria de investir hoje?"
     context.bot.send_message(chat_id=chat_id, text=message)
 
-def echo(update: Update, context):
+def invest(update: Update, context):
     chat_id = update.effective_chat.id
     message_text = update.message.text.lower()
-    numero_telefone = str(update.message.from_user.id)
 
-    if numero_telefone not in numeros_cadastrados:
-        cadastrar_numero(numero_telefone)
-
-    user_state = numeros_cadastrados[numero_telefone]["state"]
-    confirmado = numeros_cadastrados[numero_telefone]["confirmado"]
-
-    if user_state == "inicio":
-        if confirmado:
-            message = "Seu número já está cadastrado e confirmado."
-        elif "oi" in message_text or "olá" in message_text:
-            numeros_cadastrados[numero_telefone]["state"] = "btc_dolar"
-            btc_price = get_binance_price("BTCBRL")
-            usd_price = get_binance_price("BRLBUSD")
-            message = f"Olá! O preço atual do Bitcoin é {btc_price} BRL e o preço do Dólar é {usd_price} BRL. Quanto você gostaria de investir hoje?"
-        else:
-            message = "Olá! Para iniciar, por favor, digite 'oi' ou 'olá'."
-    elif user_state == "btc_dolar":
+    if message_text == 'não' or message_text == 'agradecido':
+        message = "Tudo bem, por nada! Estou aqui para ajudar. Se tiver alguma outra pergunta, é só me perguntar!"
+        context.bot.send_message(chat_id=chat_id, text=message)
+    else:
         try:
             investment_value = float(message_text)
-            btc_price = get_binance_price("BTCBRL")
-            btc_quantity = calculate_investment(investment_value, btc_price)
-            message = f"Com o valor de R${investment_value:,.2f}, você adquirirá {format_btc_value(btc_quantity)} BTC. Compre Bitcoin e Dólar sem taxa de câmbio aqui no Binance.com."
-            message += "\n\n[Visite o Binance.com](https://seu-link.com)"
-            numeros_cadastrados[numero_telefone]["state"] = "finalizado"
+            btc_price = float(client.get_avg_price(symbol="BTCBRL")['price'])
+            usdt_price = float(client.get_avg_price(symbol="USDTBRL")['price'])
+            btc_quantity = investment_value / btc_price
+            usdt_quantity = investment_value / usdt_price
+            message = f"Com o valor de {format_brl_value(investment_value)}, você adquirirá:\n"
+            message += f"{format_btc_value(btc_quantity)} BTC\n"
+            message += f"{format_brl_value(usdt_quantity)} USDT\n"
+            message += "Compre Bitcoin e Dólar sem taxa de câmbio aqui no Binance.com."
+            context.bot.send_message(chat_id=chat_id, text=message)
+
+            keyboard = [[InlineKeyboardButton("Binance.com", url="https://www.binance.com/pt-BR/activity/referral-entry/CPA?ref=CPA_00YRJDOZZV")]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+
+            message = "Clique no botão abaixo para acessar o Binance.com:"
+            context.bot.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
         except ValueError:
             message = "Por favor, digite um valor válido para investimento."
-    elif user_state == "finalizado":
-        message = "Obrigado por utilizar nosso serviço!"
+            context.bot.send_message(chat_id=chat_id, text=message)
+
+def notify():
+    chat_id = globals().get('chat_id')
+
+    btc_price = float(client.get_avg_price(symbol="BTCBRL")['price'])
+    if btc_price > last_price:
+        message = f"O preço do Bitcoin subiu para {format_brl_value(btc_price)} BRL. Vamos vender agora?"
+    elif btc_price < last_price:
+        message = f"O preço do Bitcoin baixou para {format_brl_value(btc_price)} BRL. Vamos comprar agora?"
     else:
-        message = "Comando inválido. Por favor, tente novamente."
+        return  # Não enviar notificação se o preço não mudou
 
     context.bot.send_message(chat_id=chat_id, text=message)
+    last_price = btc_price
+
+    # Agendar a função notify() para ser executada a cada hora
+    schedule.every(1).hours.do(notify)
+
+    # Executar o agendador em segundo plano
+    while True:
+             schedule.run_pending()
+             time.sleep(1)   
 
 def main():
-    token = '6002886491:AAHWbdB2DyExhvnIWJOQxkd26eP7FDCP75Q'
-    url = f'https://api.telegram.org/bot{token}/getMe'
-
-    httpx_client = httpx.Client()
-    response = httpx_client.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
-    else:
-        print(f'Request failed with status code {response.status_code}')
-        
     bot = Bot(token=TELEGRAM_TOKEN)
-    update_queue = Queue()
-    dispatcher = Dispatcher(bot, update_queue)
+    updater = Updater(bot=bot, use_context=True)
 
     start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    invest_handler = MessageHandler(Filters.text & (~Filters.command), invest)
 
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(invest_handler)
 
-    # Configuração do httpx com tamanho de pool de conexões ajustado
-    httpx._config.DEFAULT_MAX_KEEPALIVE_CONNECTIONS = 8
-    httpx._config.DEFAULT_MAX_CONNECTIONS = 40
+    # Agenda a verificação do preço a cada hora
+    job_queue = updater.job_queue
+    job_queue.run_repeating(check_price, interval=3600, first=0, context=updater.bot.get_me().id)
 
-    httpx_client = httpx.Client()
-    response = httpx_client.get('https://api.telegram.org/6002886491:AAHWbdB2DyExhvnIWJOQxkd26eP7FDCP75Q/getMe')
-    print(response.json())
-
-    updater = Updater(bot=bot, use_context=True)
     updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
+
     main()
